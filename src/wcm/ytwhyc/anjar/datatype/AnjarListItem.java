e2e5b06c2d3606f36a5a039a1de434d52cc9bf9f
ChengYan Yang <e98877331@gmail.com>
2013-07-26 00:09:37 +0800

    adding anjarListItem

---
+++ src/wcm/ytwhyc/anjar/datatype/AnjarListItem.java
@@ -0,0 +1,11 @@
+package wcm.ytwhyc.anjar.datatype;
+
+public class AnjarListItem {
+	public String anjarID,hostName,topic,hostMessage,startTime,lastUpdateTime;
+	
+	public AnjarListItem(String anjarID,String hostName, String topic, String hostMessage,String startTime, String lastUpdateTime)
+	{
+		
+	}
+	
+}
